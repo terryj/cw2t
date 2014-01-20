44b2c4643c2cd302a23b397a36ca33453872f859
terryj <terrymjohnston@gmail.com>
2014-01-20 14:56:17 +0000

    backoffice

--- backoffice.js
+++ backoffice.js
@@ -47,7 +47,7 @@ var smtpTransport = nodemailer.createTransport("SMTP",{
    service: "Gmail",
    auth: {
        user: "terrymjohnston@gmail.com",
-       pass: "***"
+       pass: "dorothy0102"
    }
 });
 
