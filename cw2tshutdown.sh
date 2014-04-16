ffb1d1998bc78e89070ccd66d3ca5332ca32b516
yearstretch <paul.weighell@yearstretch.com>
2014-04-16 13:09:01 +0100

    updates to batch files
    remvoed blank lines

--- cw2tshutdown.sh
+++ cw2tshutdown.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
-
-# ensure root
-cd
-
 #- stop trade server
-cd cw2t
 ../node_modules/forever/bin/forever stop tradeserver.js
-
-#- stop manager (optional)
+#- stop manager
 ../node_modules/forever/bin/forever stop manager.js
-
 #- stop price server
-# command goes here when I have worked out what it might be!
-
+# ???
 #stop redis server
+cd
 sudo /etc/init.d/redis_6379 stop
