80e0061eecd9a652cafa360146e8f2ba1d7db2d4
yearstretch <paul.weighell@yearstretch.com>
2014-04-16 22:31:43 +0100

    update test

--- cw2tshutdown.sh
+++ cw2tshutdown.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-#- stop trade server
 ../node_modules/forever/bin/forever stop tradeserver.js
-#- stop manager
 ../node_modules/forever/bin/forever stop manager.js
-#- stop price server
-# ???
-#stop redis server
 ../etc/init.d/redis_6379 stop
