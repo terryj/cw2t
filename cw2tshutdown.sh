05ee280620de11eb3f96d0d9efb044e7ca02c63a
yearstretch <paul.weighell@yearstretch.com>
2014-04-23 18:28:13 +0100

    update stop sh

--- cw2tshutdown.sh
+++ cw2tshutdown.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-cd
-sudo /node_modules/forever/bin/forever stop tradeserver.js
-sudo /node_modules/forever/bin/forever stop manager.js
-sudo /etc/init.d/redis_6379 stop
+../node_modules/forever/bin/forever stop tradeserver.js
+../node_modules/forever/bin/forever stop manager.js
+
