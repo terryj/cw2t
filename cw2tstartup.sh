96e4c17aa46a9e94fdaa9abd31e5ae99392d3369
yearstretch <paul.weighell@yearstretch.com>
2014-04-24 00:04:39 +0100

    test updates

--- cw2tstartup.sh
+++ cw2tstartup.sh
@@ -1,4 +1,1 @@
-../node_modules/forever/bin/forever stop tradeserver.js
-../node_modules/forever/bin/forever stop manager.js
-../node_modules/forever/bin/forever start tradeserver.js
 ../node_modules/forever/bin/forever start manager.js
